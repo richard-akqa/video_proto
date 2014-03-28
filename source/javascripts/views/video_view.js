836ad72cd50877b96ff3bd7d33132a150fa3659e
Richard T. Widjaja <richard.tirta@akqa.com>
2014-03-28 12:23:55 -0700

    videojs

--- source/javascripts/views/video_view.js
+++ source/javascripts/views/video_view.js
@@ -3,32 +3,33 @@ VIDEO.VIEW = (function(window){
 
 	var view = {};
 
-	view.loadVideo = function(node){
-		var videoAttr = 'controls width="640" height="265"',
-			vidUrl = $(node).data("vid"),
-			videoSrc =  '<source src="' + vidUrl + '.mp4" type="video/mp4"/><source src="' + vidUrl + '.webm" type="video/webm"/>',
-			videoTemplate = '<video id="video" class="video-js vjs-default-skin"' + videoAttr+ '> ' + videoSrc+ '</video>';
-
-		
-		$("#video-container").html("");
-		$("#video-container").html(videoTemplate, function(){
-			videojs("#video");
-		});
-
-	}
-
 	view.listenChannelClick = function(){
 
-		$(".video-link").click(function(){
-			view.loadVideo(this);
+		var myPlayer = videojs("video")
+
+		$("#play").click(function(){
+			myPlayer.play();
 		});
 
 		$("#time").click(function(){
-			videojs("#video").currentTime(555);
+
+			myPlayer.currentTime(100);
+
+			timeRange = myPlayer.buffered();
+
+			console.log(timeRange);
+
+			
+		})
+
+		$("#event").click(function(){
+			timeRange = myPlayer.buffered();
 		})
 	};
 
 	view.init = function(){
+		var myPlayer = videojs("video");
+
 		view.listenChannelClick();
 	};
 
