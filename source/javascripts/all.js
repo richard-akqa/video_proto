072f68d5b5b196cad7d727825f79a673d1f45e3c
Richard T. Widjaja <richard.tirta@akqa.com>
2014-03-31 00:11:02 -0700

    bug fix with audio for desktop

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,5 +3,4 @@
 //= require ./vendor/video
 //= require ./vendor/videojs.playlist
 
-//= require ./views/main_view
 //= require ./views/video_view
--- source/javascripts/views/video_view.js
+++ source/javascripts/views/video_view.js
@@ -3,122 +3,124 @@ VIDEO.VIEW = (function(window){
 
 	var view = {};
 
-	view.listenChannelClick = function(){
-
-		var vidPlayer = videojs("video"),
-			audioPlayer = document.getElementById('audio'),
-			vol = 0,
-			isFirstPlay = false;
-
-		function seekChannel(time){
-
-			var channel = undefined;
-
-			function onProgressSeek(){
-
-				vidPlayer.one("progress", function(){
-					vidPlayer.currentTime(time);
-					isFirstPlay = true;
-				});
-
-			}
-
-			currentTime = vidPlayer.currentTime();
-
-			if(!MAIN.VIEW._isAnyMobile){
-				if(currentTime == 0){
-					vidPlayer.play();
-					onProgressSeek();
-				} else {
-					vidPlayer.currentTime(time);
-				}
-			} else {
-				vidPlayer.play();	
-				if(isFirstPlay == false){
-					//this is for first play in mobile
-					vidPlayer.on("canplay", function(){
-						if(!isFirstPlay){
-							onProgressSeek();
-						}
-					});
-				} else {
-					//this is for 2nd play in mobile
-					onProgressSeek();
-				}
-			}
+	var _ua = navigator.userAgent
+		, _isMobile = {
+			Android: _ua.match(/Android/i)
+			,BlackBerry: _ua.match(/BlackBerry/i)
+			,iOS: _ua.match(/iPhone|iPod|iPad/i)
+			,Opera: _ua.match(/Opera Mini/i)
+			,Windows: _ua.match(/IEMobile/i)
+			,Smartphone: _ua.match(/Android|BlackBerry|iPhone|iPod|Opera Mini|IEMobile/i)
 		}
+		, _isAnyMobile = _isMobile.Android || _isMobile.BlackBerry  || _isMobile.iOS  || _isMobile.Opera || _isMobile.Windows
+		, _fadeInInterval
+		, _fadeOutInterval
+		, _vol = 0
+		, _isFirstPlay = false;
 
-		// var audioFadeOut = setInterval(function(){
-		// 		if(vol > 0){
-		// 			vol -= 0.05
-		// 			console.log("fading out");
-		// 			//audioPlayer.volume = vol;
-		// 		} else {
-		// 			clearInterval(audioFadeOut);
-		// 		}
-		// 	}, 200);
-		
+	view.audioFadeIn = function(audioPlayer){
+		clearInterval(_fadeOutInterval);
+		_fadeInInterval = setInterval(function(){
+			if(_vol < 10){
+				_vol += 1
+				audioPlayer.volume = _vol/10;
+			} else {
+				clearInterval(_fadeInInterval);
+			}
+		}, 100);
+	}
 
-		// var audioFadeIn = setInterval(function(){
-		// 		if(vol < 100){
-		// 			vol += 0.05
-		// 			console.log("fading in");
-		// 			//audioPlayer.volume = vol;
-		// 		} else {
-		// 			clearInterval(audioFadeIn);
-		// 		}
-		// 	}, 200);
-		
+	view.audioFadeOut = function(audioPlayer){
+		clearInterval(_fadeInInterval);
+		_fadeOutInterval = setInterval(function(){
+			if(_vol > 0){
+				_vol -= 1
+				audioPlayer.volume = _vol/10;
+			} else {
+				clearInterval(_fadeOutInterval);
+			}
+		}, 100);
+	}
+
+	view.audioTransitionInit = function(vidPlayer){
+
+		var audioPlayer = document.getElementById('audio');
 
 		audioPlayer.play();
-		audioPlayer.volume = 0;
+		audioPlayer.volume = _vol;
 
-		$("#play").click(function(){
+		vidPlayer.on("seeking", function(){
+			view.audioFadeIn(audioPlayer);
+		})
+
+		vidPlayer.on("canplay", function(){
+			view.audioFadeOut(audioPlayer);
+		})
+	}
+
+	view.onSeekChannel = function(time){
+		var vidPlayer = videojs("video"),
+			channel = undefined;
+
+		function onProgressSeek(){
+
+			vidPlayer.one("progress", function(){
+				vidPlayer.currentTime(time);
+				_isFirstPlay = true;
+			});
+		}
+
+		currentTime = vidPlayer.currentTime();
+
+		if(!_isAnyMobile){
+			if(currentTime == 0){
+				vidPlayer.play();
+				onProgressSeek();
+				view.audioTransitionInit(vidPlayer);
+			} else {
+				vidPlayer.currentTime(time);
+			}
+		} else {
 			vidPlayer.play();
+			if(!_isFirstPlay){
+				//this is for first play in mobile
+				vidPlayer.on("canplay", function(){
+					onProgressSeek();
+					vidPlayer.play();
+					view.audioTransitionInit(vidPlayer);
+				});
+			} else {
+				//this is for 2nd play in mobile
+				console.log("hello2");
+				onProgressSeek();
+			}
+		}
+	}
 
-			isFirstPlay = true;
-		});
+	view.listenChannelClick = function(){
 
 		$("#time").click(function(){
-			seekChannel(50);
+			view.onSeekChannel(50);
 		})
 
 		$("#time2").click(function(){
-			seekChannel(70);
+			view.onSeekChannel(70);
 		})
 
 		$("#time3").click(function(){
-			seekChannel(100);
+			view.onSeekChannel(100);
 		})
 
 		$("#time4").click(function(){
-			seekChannel(110);
+			view.onSeekChannel(110);
 		})
 
 		$("#time5").click(function(){
-			seekChannel(130);
+			view.onSeekChannel(130);
 		})
-
-		// vidPlayer.on("seeking", function(){
-		// 	console.log("seeking")
-		// 	$("#status").html("seeking");
-		// 	audioPlayer.volume = 0;
-		// 	//clearInterval(audioFadeOut);
-		// 	//audioFadeIn(audioPlayer);
-		// })
-
-		// vidPlayer.on("canplay", function(){
-		// 	console.log("canplay")
-		// 	$("#status").html("starting");
-		// 	audioPlayer.volume = 0;
-		// 	//clearInterval(audioFadeIn);
-		// 	//audioFadeOut(audioPlayer);
-		// })
-
 	};
 
 	view.init = function(){
-
 		view.listenChannelClick();
 	};
 
